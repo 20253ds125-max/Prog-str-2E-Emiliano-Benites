f920f14ec83b774c1198f724e6aa453c074edbb2
Omar Emiliano Hernandez Benites <20253ds125@utez.edu.mx>
2026-01-12 12:33:23 -0600

    Creacion de codigo

---
+++ Hola mundo/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.printf("Hola mundo ");
+        System.out.printf("Omar Emiliano Hernandez Benites ");
+        System.out.printf("2-E ");
+        System.out.printf("12-1-2026");
+
+    }
+}
